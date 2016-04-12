4c9b1108ae4cc057a748b5e3e4a3a0efdee06b36
Alejandro Gutierrez Gongora <alex.gtz17@gmail.com>
2016-04-12 14:33:50 -0600

    Tests for cell passing

---
+++ csharp/GameOfLife/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Cell
+    {
+        public bool status { get; set; }
+
+        public Cell()
+        {
+            status = false;
+        }
+    }
+}
---
+++ csharp/GameOfLifeTests/CellTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameOfLife;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class CellTests
+    {
+        [TestMethod]
+        public void CellInstantiation()
+        {
+            // Arrange
+            Cell cell = new Cell();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(cell);
+        }
+        [TestMethod]
+        public void CellInitStatusIsFalse()
+        {
+            // Arrange
+            Cell cell = new Cell();
+
+            // Act
+            var expected = false;
+            var actual = cell.status;
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void CellChangeStatusToAlive()
+        {
+            // Arrange
+            Cell cell = new Cell();
+
+            // Act
+            var expected = true;
+            cell.status = true;
+            var actual = cell.status;
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
