746fd90fdc095e6685ee237b3128dc7e1fe90c1b
Alejandro Gutierrez Gongora <alex.gtz17@gmail.com>
2016-04-12 18:57:06 -0600

    checking neighbors test passing

--- csharp/GameOfLife/Gameboard.cs
+++ csharp/GameOfLife/Gameboard.cs
@@ -22,5 +22,60 @@ namespace GameOfLife
                 }
             }
         }
+
+        public void checkNeighbors()
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    int count = 0;
+                    int left = mod((row - 1), 10);
+                    int right = mod((row + 1), 10);
+                    int bottom = mod((col - 1), 10);
+                    int top = mod((col + 1), 10);
+
+                    if (this.actualStatusGrid[left, top].status) // left top corner
+                    {
+                        count++;
+                    }
+                    if (this.actualStatusGrid[row, top].status) // top
+                    {
+                        count++;
+                    }
+                    if (this.actualStatusGrid[right, top].status) // top right corner
+                    {
+                        count++;
+                    }
+                    if (this.actualStatusGrid[right, col].status) // right
+                    {
+                        count++;
+                    }
+                    if (this.actualStatusGrid[right, bottom].status) // right bottom corner
+                    {
+                        count++;
+                    }
+                    if (this.actualStatusGrid[row, bottom].status) // bottom
+                    {
+                        count++;
+                    }
+                    if (this.actualStatusGrid[left, bottom].status) // left buttom corner
+                    {
+                        count++;
+                    }
+                    if (this.actualStatusGrid[left, col].status) // left
+                    {
+                        count++;
+                    }
+                    this.actualStatusGrid[row, col].neighbors = count;
+                }
+            }
+        }
+
+        int mod(int x, int m)
+        {
+            return (x % m + m) % m;
+        }
+
     }
 }
--- csharp/GameOfLifeTests/GameboardTests.cs
+++ csharp/GameOfLifeTests/GameboardTests.cs
@@ -25,12 +25,43 @@ namespace GameOfLifeTests
             Gameboard gameboard = new Gameboard();
 
             // Act
-            var actual = gameboard.actualStatusGrid[2, 2];
-            var statusValue = actual.status;
+            var statusValue = gameboard.actualStatusGrid[2, 2];
+            var actual = statusValue.status;
             var expected = false;
 
             // Assert
-            Assert.AreEqual(statusValue, expected);
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void GameboardCellCanBeTrue()
+        {
+            // Arrange
+            Gameboard gameboard = new Gameboard();
+
+            // Act
+            gameboard.actualStatusGrid[0, 0].status = true;
+            var actual = gameboard.actualStatusGrid[0, 0].status;
+            var expected = true;
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void GameboardCheckForNeighbors()
+        {
+            // Arrange
+            Gameboard gameboard = new Gameboard();
+
+            // Act
+            gameboard.actualStatusGrid[2, 2].status = true;
+            gameboard.actualStatusGrid[2, 3].status = true;
+            gameboard.actualStatusGrid[2, 4].status = true;
+            gameboard.checkNeighbors();
+            var actual = gameboard.actualStatusGrid[3, 3].neighbors;
+            var expected = 3;
+
+            // Assert
+            Assert.AreEqual(actual, expected);
         }
     }
 }
