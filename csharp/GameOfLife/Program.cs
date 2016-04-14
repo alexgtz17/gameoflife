7f203d983f65ad4b2b5e52d4aa3e2620242ea808
Alejandro Gutierrez Gongora <alex.gtz17@gmail.com>
2016-04-13 18:14:21 -0600

    glider working in console

--- csharp/GameOfLife/Program.cs
+++ csharp/GameOfLife/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace GameOfLife
 {
@@ -10,6 +11,22 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
+            var gameboard = new Gameboard();
+            gameboard.actualStatusGrid[5, 5].status = true;
+            gameboard.actualStatusGrid[6, 5].status = true;
+            gameboard.actualStatusGrid[7, 5].status = true;
+            gameboard.actualStatusGrid[7, 4].status = true;
+            gameboard.actualStatusGrid[6, 3].status = true;
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(gameboard.convertGridToString());
+                gameboard.checkNeighbors();
+                Thread.Sleep(500);
+                gameboard.nextGen();
+            }           
+
         }
     }
 }
